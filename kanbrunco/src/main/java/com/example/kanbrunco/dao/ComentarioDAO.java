583b7a05eb30d2154fcd14793536edf9edeb873a
julio-cesar2606 <cesar.nascimento@dcomp.ufs.br>
2025-08-21 14:31:17 -0300

    ComentarioDAO.java

--- kanbrunco/src/main/java/com/example/kanbrunco/dao/ComentarioDAO.java
+++ kanbrunco/src/main/java/com/example/kanbrunco/dao/ComentarioDAO.java
@@ -1,5 +1,12 @@
 package com.example.kanbrunco.dao;
 
+import com.example.kanbrunco.model.comentario.Comentario;
+import java.time.DateTime;
+import java.io*;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public class ComentarioDAO {
+  
 
 }
