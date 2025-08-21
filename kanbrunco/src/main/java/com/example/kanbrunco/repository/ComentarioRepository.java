8e147e6e3d023e5e50313f189a58e2735209e00e
EvilasioNtt <evilasiocavalcantemeloneto@gmail.com>
2025-08-20 23:42:50 -0300

    Update ComentarioRepository.java

--- kanbrunco/src/main/java/com/example/kanbrunco/repository/ComentarioRepository.java
+++ kanbrunco/src/main/java/com/example/kanbrunco/repository/ComentarioRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.example.kanbrunco.model.comentario.Comentario;
 
 @Repository
-public class ComentarioRepository extends JpaRepository<Comentario, Long> {
+public interface ComentarioRepository extends JpaRepository<Comentario, Long> {
 
 }
