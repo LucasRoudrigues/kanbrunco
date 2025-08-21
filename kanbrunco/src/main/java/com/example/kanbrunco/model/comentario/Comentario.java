2fc501533e196d47fd1a2a164981a61e79a34fbf
julio-cesar2606 <cesar.nascimento@dcomp.ufs.br>
2025-08-21 14:17:46 -0300

    Comentario.java
    adições de atributos para ajudar na linkagem entre cartão e comentário

--- kanbrunco/src/main/java/com/example/kanbrunco/model/comentario/Comentario.java
+++ kanbrunco/src/main/java/com/example/kanbrunco/model/comentario/Comentario.java
@@ -1,19 +1,28 @@
 package com.example.kanbrunco.model.comentario;
 
 import com.example.kanbrunco.model.usuario.Usuario;
+import com.example.kabrunco.model.cartao.Cartao;
 import java.time.LocalDateTime;
 
 // Esta classe representa um comentário em um cartão.
 public class Comentario {
 
+    private Long id;
     private String texto;
     private Usuario autor;
     private LocalDateTime dataCriacao;
+    private Cartao cartao;
 
-    public Comentario(String texto, Usuario autor) {
+    public Comentario(Long id,String texto, Usuario autor, Cartao cartao) {
+        this.id=id;
+        this.cartao= cartao;
         this.texto = texto;
         this.autor = autor;
         this.dataCriacao = LocalDateTime.now(); // Define a data de criação automaticamente.
+    }
+
+    public Long getId(){
+        return id;
     }
     public String getTexto() {
         return texto;
@@ -21,6 +30,10 @@ public class Comentario {
 
     public Usuario getAutor() {
         return autor;
+    }
+
+    public Cartao getCartao(){
+        return cartao;
     }
 
     public LocalDateTime getDataCriacao() {
