2d7a0d5a575334d29411821929d8937abf79e9bf
EvilasioNtt <evilasiocavalcantemeloneto@gmail.com>
2025-08-20 23:33:59 -0300

    Repositorio Quadro usando spring

--- kanbrunco/src/main/java/com/example/kanbrunco/repository/QuadroRepository.java
+++ kanbrunco/src/main/java/com/example/kanbrunco/repository/QuadroRepository.java
@@ -1,5 +1,11 @@
 package com.example.kanbrunco.repository;
 
-public class QuadroRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.kanbrunco.model.quadro.Quadro;
+
+@Repository
+public interface QuadroRepository extends JpaRepository<Quadro, Long> {
 
 }
