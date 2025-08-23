239538fe12f0089f1e9a6c3832a46ff668db1558
julio-cesar2606 <cesar.nascimento@dcomp.ufs.br>
2025-08-23 20:08:36 -0300

    QuadroService.java
    atualização do quadro service, salvar quadro baseado no tipo

--- kanbrunco/src/main/java/com/example/kanbrunco/service/QuadroService.java
+++ kanbrunco/src/main/java/com/example/kanbrunco/service/QuadroService.java
@@ -1,7 +1,6 @@
 package com.example.kanbrunco.service;
 
-import com.example.kanbrunco.model.quadro.Quadro;
-import com.example.kanbrunco.model.quadro.QuadroCompartilhado;
+import com.example.kanbrunco.model.quadro.*;
 import com.example.kanbrunco.model.usuario.Usuario;
 import com.example.kanbrunco.repository.QuadroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +19,23 @@ public class QuadroService {
     public Quadro salvarQuadro(Quadro quadro) {
         return quadroRepository.save(quadro);
     }
+    
+    
+    public Quadro salvarQuadroNormal(String titulo, Usuario proprietario) {
+        QuadroNormal quadro = new QuadroNormal();
+        quadro.setTitulo(titulo);
+        quadro.setProprietario(proprietario);
+        return quadroRepository.save(quadro);
+    }
 
+
+    public Quadro salvarQuadroCompartilhado(String titulo, Usuario proprietario) {
+        QuadroCompartilhado quadro = new QuadroCompartilhado();
+        quadro.setTitulo(titulo);
+        quadro.setProprietario(proprietario);
+        return quadroRepository.save(quadro);
+    }
+    
     public List<Quadro> listarTodos() {
         return quadroRepository.findAll();
     }
