0d1f7b7f8c98d4dd0127e95a6152bd347c2a8fb7
julio-cesar2606 <cesar.nascimento@dcomp.ufs.br>
2025-08-23 12:30:31 -0300

    CartaoSimples.java
    Alteração de todo o model cartao para utilizar as dependencias que estão no pom.xml

--- kanbrunco/src/main/java/com/example/kanbrunco/model/cartao/CartaoSimples.java
+++ kanbrunco/src/main/java/com/example/kanbrunco/model/cartao/CartaoSimples.java
@@ -1,22 +1,12 @@
 package com.example.kanbrunco.model.cartao;
 
-import com.example.kanbrunco.model.cartao.Cartao;
+import jakarta.persistence.*;
 
+@Entity
+@DiscriminatorValue("simples")
 public class CartaoSimples extends Cartao {
-
-    public CartaoSimples(){
+    
+    public CartaoSimples() {
         super();
-    }
-
-    public CartaoSimples(Long id, String titulo, String descricao, Long idquadro) {
-        super(id, titulo, descricao, idquadro);
-    }
-
-    @Override
-    public void exibirDetalhes() {
-        System.out.println("--- Cartão Simples ---");
-        System.out.println("Título: " + getTitulo());
-        System.out.println("Descrição: " + getDescricao());
-        System.out.println("Comentários (" + getComentarios().size() + ")");
     }
 }
