7b88c3796be64339e4d806bcbb56ace9d99a5c62
julio-cesar2606 <cesar.nascimento@dcomp.ufs.br>
2025-08-23 20:10:10 -0300

    ComentarioService.java
    atualização para inserir e remover fazendo a conexão com o cartão

--- kanbrunco/src/main/java/com/example/kanbrunco/service/ComentarioService.java
+++ kanbrunco/src/main/java/com/example/kanbrunco/service/ComentarioService.java
@@ -3,6 +3,7 @@ package com.example.kanbrunco.service;
 import com.example.kanbrunco.model.comentario.Comentario;
 import com.example.kanbrunco.model.cartao.Cartao;
 import com.example.kanbrunco.repository.ComentarioRepository;
+import com.example.kanbrunco.repository.CartaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,10 @@ public class ComentarioService {
     @Autowired
     private ComentarioRepository comentarioRepository;
 
+    @Autowired
+    private CartaoRepository cartaoRepository;
+    
+    //Salvar Comentarios
     public Comentario salvarComentario(Comentario comentario) {
         return comentarioRepository.save(comentario);
     }
@@ -23,6 +28,7 @@ public class ComentarioService {
         return comentarioRepository.findAll();
     }
 
+    //Métodos de Busca
     public Optional<Comentario> buscarPorId(Long id) {
         return comentarioRepository.findById(id);
     }
@@ -33,5 +39,24 @@ public class ComentarioService {
 
     public void deletarComentario(Long id) {
         comentarioRepository.deleteById(id);
+    }
+    
+    //Métodos de adição e remoção de comentarios fazendo a conexão com seu devido cartão
+    public Comentario adicionarComentario(Long cartaoId, Comentario comentario) {
+        Optional<Cartao> cartaoOptional = cartaoRepository.findById(cartaoId);
+        if (cartaoOptional.isPresent()) {
+            Cartao cartao = cartaoOptional.get();
+            comentario.setCartao(cartao);
+            return comentarioRepository.save(comentario);
+        }
+        return null;		// Retorna nulo caso não ache o cartão
+    }
+
+    public void removerComentario(Long comentarioId) {
+        Optional<Comentario> comentarioOptional = comentarioRepository.findById(comentarioId);
+        if (comentarioOptional.isPresent()) {
+            Comentario comentario = comentarioOptional.get();
+            comentarioRepository.delete(comentario);
+        }
     }
 }
