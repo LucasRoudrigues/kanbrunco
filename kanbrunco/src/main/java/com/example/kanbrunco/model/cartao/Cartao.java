95a50f9c8a51676af258838a713ac2da89bd6839
julio-cesar2606 <cesar.nascimento@dcomp.ufs.br>
2025-08-23 12:29:02 -0300

    Cartao.java

--- kanbrunco/src/main/java/com/example/kanbrunco/model/cartao/Cartao.java
+++ kanbrunco/src/main/java/com/example/kanbrunco/model/cartao/Cartao.java
@@ -1,34 +1,43 @@
 package com.example.kanbrunco.model.cartao;
 
 import com.example.kanbrunco.model.comentario.Comentario;
+import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "tipo_cartao", discriminatorType = DiscriminatorType.STRING)
 public abstract class Cartao {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String titulo;
     private String descricao;
-    private List<Comentario> comentarios;
-    private Long idquadro;
+    
+    // Mapeamento para a lista de comentarios e conexão de um cartão possuindo varios comentarios
+    @OneToMany(mappedBy = "cartao", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comentario> comentarios = new ArrayList<>();
 
-    public Cartao(){
+    @Column(name = "id_quadro")
+    private Long idQuadro;
+
+    public Cartao() {
     }
 
-    public Cartao(Long id, String titulo, String descricao, Long idquadro) {
-        this.id = id;
-        this.titulo = titulo;
-        this.descricao = descricao;
-        this.comentarios = new ArrayList<>();
-        this.idquadro = idquadro;
-    }
-
-    // Método abstrato para polimorfismo, a ser implementado nas subclasses.
-    public abstract void exibirDetalhes();
-
-    // Método para adicionar um comentário ao cartão.
+    // Método para adicionar um comentário ao cartão
     public void adicionarComentario(Comentario comentario) {
+        comentario.setCartao(this);
         this.comentarios.add(comentario);
+    }
+
+    // Método para remover um comentário do cartão
+    public void removerComentario(Comentario comentario) {
+        if (this.comentarios.contains(comentario)) {
+            this.comentarios.remove(comentario);
+            comentario.setCartao(null);
+        }
     }
 
     public Long getId() {
@@ -59,11 +68,15 @@ public abstract class Cartao {
         return comentarios;
     }
 
-    public Long getIdQuadro() {
-        return idquadro;
+    public void setComentarios(List<Comentario> comentarios) {
+        this.comentarios = comentarios;
     }
 
-    public void setIdQuadro(Long idquadro) {
-        this.idquadro = idquadro;
+    public Long getIdQuadro() {
+        return idQuadro;
+    }
+
+    public void setIdQuadro(Long idQuadro) {
+        this.idQuadro = idQuadro;
     }
 }
