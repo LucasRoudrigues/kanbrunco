0f907d5aa445bd5243c5cc37a73c8b79dce9fc1c
julio-cesar2606 <cesar.nascimento@dcomp.ufs.br>
2025-08-23 20:11:26 -0300

    CartaoService.java
    salvar o cartão de acordo com seu tipo

--- kanbrunco/src/main/java/com/example/kanbrunco/service/CartaoService.java
+++ kanbrunco/src/main/java/com/example/kanbrunco/service/CartaoService.java
@@ -1,6 +1,6 @@
 package com.example.kanbrunco.service;
 
-import com.example.kanbrunco.model.cartao.Cartao;
+import com.example.kanbrunco.model.cartao.*;
 import com.example.kanbrunco.model.lista.ListaTarefas;
 import com.example.kanbrunco.repository.CartaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +15,31 @@ public class CartaoService {
     @Autowired
     private CartaoRepository cartaoRepository;
 
+    //Métodos para salvar os cartões
     public Cartao salvarCartao(Cartao cartao) {
         return cartaoRepository.save(cartao);
     }
 
+    public Cartao salvarCartaoSimples(String titulo, String descricao) {
+        CartaoSimples cartao = new CartaoSimples();
+        cartao.setTitulo(titulo);
+        cartao.setDescricao(descricao);
+        return cartaoRepository.save(cartao);
+    }
+
+    public Cartao salvarCartaoComChecklist(String titulo, String descricao) {
+        CartaoComChecklist cartao = new CartaoComChecklist();
+        cartao.setTitulo(titulo);
+        cartao.setDescricao(descricao);
+        return cartaoRepository.save(cartao);
+    }
+    
+    //Listar Cartões
     public List<Cartao> listarTodos() {
         return cartaoRepository.findAll();
     }
 
+    //Métodos de Busca e Remoção
     public Optional<Cartao> buscarPorId(Long id) {
         return cartaoRepository.findById(id);
     }
