7a0f9e60eaac56ee2275e5d49ac97c35eecda99b
julio-cesar2606 <cesar.nascimento@dcomp.ufs.br>
2025-08-23 21:31:54 -0300

    UsuarioController.java

--- kanbrunco/src/main/java/com/example/kanbrunco/controller/UsuarioController.java
+++ kanbrunco/src/main/java/com/example/kanbrunco/controller/UsuarioController.java
@@ -1,1 +1,66 @@
+package com.example.kanbrunco.controller;
 
+import com.example.kanbrunco.model.usuario.*;
+import com.example.kanbrunco.service.UsuarioService; 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService; 
+
+    public UsuarioController(UsuarioService usuarioService) { 
+        this.usuarioService = usuarioService;
+    }
+
+    // Listar todos os usuários
+    @GetMapping
+    public String listarUsuarios(Model model) {
+        List<Usuario> usuarios = usuarioService.listarTodos(); 
+        model.addAttribute("usuarios", usuarios);
+        return "usuarios/lista";
+    }
+
+    //Métodos para criação de usuarios 
+    @PostMapping("/criarBasico")
+    public String criarUsuarioBasico(@RequestParam String nome, @RequestParam String email) {
+        usuarioService.salvarUsuarioBasico(nome, email);
+        return "redirect:/usuarios";
+    }
+
+    
+    @PostMapping("/criarAdmin")
+    public String criarUsuarioAdmin(@RequestParam String nome, @RequestParam String email) {
+        usuarioService.salvarUsuarioAdmin(nome, email);
+        return "redirect:/usuarios";
+    }
+    
+    
+    // Atualizar usuário
+    @PostMapping("/atualizar")
+    public String atualizarUsuario(@RequestParam Long id,
+                                   @RequestParam String nome,
+                                   @RequestParam String email) {
+        Optional<Usuario> optionalUsuario = usuarioService.buscarPorId(id); 
+        if (optionalUsuario.isPresent()) {
+            Usuario usuario = optionalUsuario.get();
+            usuario.setNome(nome);
+            usuario.setEmail(email);
+            usuarioService.salvarUsuario(usuario); 
+        }
+        return "redirect:/usuarios";
+    }
+
+    // Deletar usuário
+    @PostMapping("/deletar")
+    public String deletarUsuario(@RequestParam Long id) {
+        usuarioService.deletarUsuario(id); 
+        return "redirect:/usuarios";
+    }
+}
